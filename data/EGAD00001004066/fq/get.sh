082d694d430fd43f7f6a58dd7a7f506a471d7ff4
David J. H. Shih <djh.shih@gmail.com>
2025-07-23 19:25:01 +0800

    Add notes.

--- data/EGAD00001004066/fq/get.sh
+++ data/EGAD00001004066/fq/get.sh
@@ -4,8 +4,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# NB  install pyega3 by
+#       pip install pyega3
 # NB  creditial file must be at ~/.config/ega/login
 # NB  if an error regarding tqdm occurs, upgrade tqdm by
-#     pip install tqdm --upgrade
+#       pip install tqdm --upgrade
 
 pyega3 -d -cf ~/.config/ega/login fetch EGAD00001004066
+
