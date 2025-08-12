cfdafd0721a3d8c1039d363b4541904479929218
MoyukhShabon <moyukh.work@gmail.com>
2025-08-12 21:05:54 +0800

    modified script to organize fastqs

--- EGAD00001004066/data/organize_fastqs.py
+++ EGAD00001004066/data/organize_fastqs.py
@@ -4,35 +4,28 @@ import pandas as pd
 
 target_dir_root = "fq"
 
-fastq_annotation = pd.read_csv("../annot/sample_file.csv")
-fastq_annotation
-
-sample_annotation = pd.read_csv("../annot/samples.csv")
-sample_annotation
-
-phenotypes = sample_annotation[["alias", "phenotype"]]
-
-fastq_annotation_new = pd.merge(fastq_annotation, phenotypes, left_on="sample_alias", right_on="alias")
-fastq_annotation_new.to_csv("../annot/sample_file_phenotype.tsv", sep="\t", index=False)
+fastq_annotation = pd.read_csv("../annot/fastq_annotations.tsv", sep="\t")
+# fastq_annotation
 
 fastq_paths = glob.glob("fq_old/*/*fastq.gz")
+# fastq_paths
 
 for fastq_path in fastq_paths:
     file_accession = fastq_path.split("/")[-2]
     
-    annot = fastq_annotation_new[fastq_annotation_new.file_accession_id == file_accession]
+    annot = fastq_annotation[fastq_annotation.fastq_accession_id == file_accession]
     
-    file_name = annot["file_name"].iloc[0]
-    phenotype = annot["phenotype"].iloc[0].replace(" ", "-")
-    alias = annot["alias"].iloc[0]
+    fq_fname = annot["fastq_file_name"].iloc[0]
+    description = annot["title"].iloc[0].replace(" ", "-")
+    alias = annot["sample_alias"].iloc[0]
     
-    target_dir = f"{target_dir_root}/{phenotype}_{alias}"
+    target_dir = f"{target_dir_root}/{description}_{alias}"
     os.makedirs(target_dir, exist_ok=True)
     
-    target_path = f"{target_dir}/{phenotype}_{alias}_{file_name}"
+    target_path = f"{target_dir}/{description}_{alias}_{fq_fname}"
     
     os.link(fastq_path, target_path)
-    print(f"Linked: \n\t{fastq_path}\nTo:\n\t{target_path}\n")
+    print(f"Linked: \n\t{fastq_path}\nTo:\n\t{target_path}\n------")
     
 print("Done.")
 
