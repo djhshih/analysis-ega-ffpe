a7ac140eb9a5d9c2d553f0a3222481dfd4d85326
MoyukhShabon <moyukh.work@gmail.com>
2025-08-12 21:10:03 +0800

    updated input preparation script for alignment

--- EGAD00001004066/analyses/fastq_align_paired/prepare.py
+++ EGAD00001004066/analyses/fastq_align_paired/prepare.py
@@ -9,32 +9,38 @@ import pandas as pd
 # ---
 
 # absolute path is required in the json input files for WDL
-ref_root = os.path.abspath('../../ref')
+ref_root = os.path.abspath('../../../common_data/ref')
 ref_fname = 'Homo_sapiens_assembly38.fasta'
 
-infname = '../../data/sample-annotations.tsv'
-rg_root = os.path.abspath('../../rg/rg')
-fq_root = os.path.abspath('../../data/')
+infname = '../../annot/fastq_annotations.tsv'
+rg_root = os.path.abspath('../../data/rg/rg')
+fq_root = os.path.abspath('../../data/fq')
 out_dir = 'inputs'
 
 
 if not os.path.exists(out_dir):
     os.makedirs(out_dir)
 
-fastq_pd = pd.read_csv(infname, sep='\t')
+fastq_pd = pd.read_csv(infname, sep="\t")
 
 # ----
 
 samples = []
-for i in range(fastq_pd.shape[0]):
-    param_id = fastq_pd.loc[i, "parameter_id"]
-    depth = fastq_pd.loc[i, "depth"]
-    fastqs_r1 = os.path.join(fq_root, f"{depth}/{param_id}/{depth}_{param_id}_ffpe_r1.fq")
-    fastqs_r2 = os.path.join(fq_root, f"{depth}/{param_id}/{depth}_{param_id}_ffpe_r2.fq")
+for _, x in fastq_pd.groupby('sample_alias'):
+    alias = x["sample_alias"].iloc[0]
+    title = x["title"].iloc[0].replace(" ", "-")
+    
+    fq1 = x["fastq_file_name"].iloc[0]
+    fq2 = x["fastq_file_name"].iloc[1]
+    
+    rg = (f"{title}_{alias}")
+    
+    fastqs_r1 = os.path.join(fq_root, f"{title}_{alias}/{title}_{alias}_{fq1}")
+    fastqs_r2 = os.path.join(fq_root, f"{title}_{alias}/{title}_{alias}_{fq2}")
     samples.append(
         {
-            'sample_id': f"{depth}_{param_id}",
-            # 'accession': run_accession,
+            'sample_id': f"{title}_{alias}",
+            "rg_name" : rg,
             'fastqs_r1': fastqs_r1,
             'fastqs_r2': fastqs_r2,
         }
@@ -65,7 +71,7 @@ for x in samples:
     out['fastq_align_paired.sample_id'] = x['sample_id']
     out['fastq_align_paired.fastq_bwa_mem_paired.fastqs_r1'] = [x['fastqs_r1']]
     out['fastq_align_paired.fastq_bwa_mem_paired.fastqs_r2'] = [x['fastqs_r2']]
-    out['fastq_align_paired.fastq_bwa_mem_paired.rg_header'] = os.path.join(rg_root, x["sample_id"] + '_ffpe.rg.txt')
+    out['fastq_align_paired.fastq_bwa_mem_paired.rg_header'] = os.path.join(rg_root, x["rg_name"] + '.rg.txt')
     out_path = os.path.join(out_dir, x['sample_id'] + '.inputs')
     with open(out_path, 'w') as outf:
         outf.write(json.dumps(out, indent=True, sort_keys=True))
