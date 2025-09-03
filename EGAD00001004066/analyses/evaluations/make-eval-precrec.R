dd370baeec7e641297408d21ae9b31526e1969f7
MoyukhShabon <moyukh.work@gmail.com>
2025-09-03 19:18:18 +0800

    eval script using only precrec

---
+++ EGAD00001004066/analyses/evaluations/make-eval-precrec.R
@@ -0,0 +1,165 @@
+#!/usr/bin/env Rscript
+
+# Import necessary libraries and modules
+library(io)
+library(precrec)
+library(jsonlite)
+library(argparser)
+library(tidyverse)
+library(glue)
+
+## Importing module containing common functions for evaluations
+source("../../../R/eval.R")
+
+# %% [markdown]
+# ### Setup Directories
+
+## Directory for FFPE SNVF outputs
+ffpe_snvf.dir <- "../../ffpe-snvf/somatic_vcf"
+## Directory for the Somatic VCFs
+vcf.dir <- "../../data/somatic_vcf"
+## Output directory
+main.outdir <- "../../evaluations2/somatic_vcf"
+
+# %% [markdown]
+# ### Read Annotation Table
+
+## Read the annotation table
+lookup_table <- read.delim("../../annot/sample_annotations.tsv") |> mutate(sample_name = glue('{gsub(" ", "-", title)}_{sample_alias}'))
+
+## Stratify annotation table based on FFPE and FF Somatic Variants
+ffpe_tumoral <- lookup_table |> filter(preservation == "FFPE", sample_type == "Tumoral")
+frozen_tumoral <- lookup_table |> filter(preservation == "Frozen", sample_type == "Tumoral")
+
+# %% [markdown]
+# ### Prepare data for evaluation
+
+prepare.eval.data <- function(ffpe_tumoral, sample_index) {
+
+	## obtain metadata for one of the samples  
+	sample_name <- ffpe_tumoral[sample_index, "sample_name"]
+	tissue <- ffpe_tumoral[sample_index, "tissue_type"]
+	variant_caller <- "MuTect2"
+	
+	# Read in the FFPE-Filtered output of a sample
+	## Read and process mobsnvf outputs
+	mobsnvf_output <- read.delim(glue("{ffpe_snvf.dir}/mobsnvf/{sample_name}/{sample_name}.mobsnvf.snv"))
+	### Retain only C>T mutations in mobsnvf output
+	mobsnvf_output <- mobsnvf_output[complete.cases(mobsnvf_output), ]
+
+
+	## Read and process vafsnvf outputs
+	vafsnvf_output <- read.delim(glue("{ffpe_snvf.dir}/vafsnvf/{sample_name}/{sample_name}.vafsnvf.snv"))
+
+
+	## Read and process sobdetector outputs
+	sobdetector_output <- read.delim(glue("{ffpe_snvf.dir}/sobdetector/{sample_name}/{sample_name}.sobdetector.snv"))
+	### Drop values where it could not determine wether an SNV was an artifact or real mutation
+	sobdetector_output <- drop_na(sobdetector_output, artiStatus)
+	### Change SOBdetector to SOB Column to numeric
+	sobdetector_output$SOB <- as.numeric(sobdetector_output$SOB)
+	### Change values where SOB score is nan to 0 as artiStatus column for these values are annotated as SNV i.e real mutation
+	### By default, higher SOB score indicates likelihood of artifact. This is why real mutations are set to 0
+	sobdetector_output$SOB <- if_else(is.nan(sobdetector_output$SOB), 0, sobdetector_output$SOB)
+
+
+	## Read and process GATK Orientation Bias Mixture Model outputs
+	gatk_obmm_output <- read_vcf(glue("{vcf.dir}/{sample_name}/{sample_name}.vcf.gz"), columns = c("chrom", "pos", "ref", "alt", "filter"))
+	### GATK Orientation Bias mixture model makes binary classification. This is casted into scores 0 and 1
+	gatk_obmm_output$obmm <- ifelse(grepl("orientation", gatk_obmm_output$filter), 0, 1)
+
+
+	# Add common IDs to the outputs
+	mobsnvf_output <- add_id(mobsnvf_output)
+	vafsnvf_output <- add_id(vafsnvf_output)
+	sobdetector_output <- add_id(sobdetector_output)
+	gatk_obmm_output <- add_id(gatk_obmm_output)
+	
+	
+	# Now combine all outputs into a single dataframe
+	## This removes C>T mutation from the output of other models
+	## This also ensures that an uniform variant set is maintained. As some may variants are omitted by some models
+	all_model_scores <- select(mobsnvf_output, chrom, pos, ref, alt, snv, FOBP)
+	all_model_scores <- left_join(all_model_scores, select(vafsnvf_output, snv, VAFF), by = "snv")
+	all_model_scores <- left_join(all_model_scores, select(sobdetector_output, snv, SOB), by = "snv")
+	all_model_scores <- left_join(all_model_scores, select(gatk_obmm_output, snv, obmm), by = "snv")
+	
+	
+	# Construct the Ground Truth SNVs
+	## To do this we make a union set for all the FF samples matched to this FFPE sample
+	## This dataset has matched FFPE and FF from the same patient.
+	## All samples within the same tissue type are matched
+	## Hence, we select the frozen variants from the same tissue type as the FFPE sample
+	frozen_tumoral_subset <- frozen_tumoral[frozen_tumoral$tissue_type == tissue, ]
+
+	## Obtain the sample names for the frozen samples within the same tissue
+	truth_samples <- frozen_tumoral_subset$sample_name
+	## Retrieve the path for the truth samples as a vector
+	truth_sample_paths <- glue("{vcf.dir}/{truth_samples}/{truth_samples}.vcf.gz")
+	## Construct a variant set from these FFPE
+	truths <- snv_union(truth_sample_paths)
+	## Add unique IDs to the truth set
+	truths <- add_id(truths)
+
+
+	# Add truth labels variants in the all_model_scores dataframe based on whether the variant is present in the ground truth
+	all_model_scores_truths <- annotate_truth(damaged_sample_variants = all_model_scores, ground_truth_variants = truths)
+
+
+	# Adjust scores
+	## In this set the TRUE label indicates real mutation and FALSE indicates artifacts
+	## Hence, scores needs to be adjusted so that higher score represents a real mutation.
+	## For our VAFSNVF higher score is signifies real mutation : 
+	## For MOBSNVF and SOBDetector lower score signifies real mutation:  
+	## Hence, we flip scores for MOBSNVF and SOBDetector to make higher score signify a real muatation
+	all_model_scores_truths$FOBP <- -all_model_scores_truths$FOBP
+	all_model_scores_truths$SOB <- -all_model_scores_truths$SOB
+
+	list(
+		sample_id = sample_name,
+		model_scores_labels_truths = all_model_scores_truths,
+		ground_truth_variants =  truths
+	)
+
+}
+
+
+print(glue("Evaluating: "))
+
+## Perform per sample evaluation
+for (index in seq_along(ffpe_tumoral)){
+
+	## Prepare the data for evaluation
+	eval_data <- prepare.eval.data(ffpe_tumoral, index)
+
+	## Retrieve sample name for the iteration
+	sample_name <- eval_data$sample_id
+	print(glue("\t{index}. {sample_name}"))
+
+	## Create output directory for saving the evaluation data for each sample
+	out_dir <- glue("{main.outdir}/model_scores_labels_truths/{sample_name}")
+	dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+	qwrite(eval_data$model_scores_labels_truths, glue("{out_dir}/{sample_name}_model_scores_labels_truths.tsv"))
+
+	## Create an output directory for saving the ground truth for each sample
+	out_dir <- glue("{main.outdir}/ground_truth/{sample_name}")
+	dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+	qwrite(eval_data$ground_truth_variants, glue("{out_dir}/{sample_name}_ground_truth_variants.tsv"))
+
+
+	# Evaluation using Precrec
+	eval_data_df <- eval_data$model_scores_labels_truths
+	precrec_eval_metrics <- get.precrec.eval.metrics(eval_data_df, score_columns = c("FOBP", "VAFF", "SOB", "obmm"), model_names = c("mobsnvf", "vafsnvf", "sobdetector", "gatk-obmm"))
+
+	## Create output directory for roc prc and auc evaluation
+	out_dir <- glue("{main.outdir}/roc-prc-auc/precrec/{sample_name}")
+	dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+
+	qwrite(precrec_eval_metrics$auc_table, glue("{out_dir}/{sample_name}_auc_table.tsv"))
+	qwrite(precrec_eval_metrics$roc, glue("{out_dir}/{sample_name}_roc_coordinates.tsv"))
+	qwrite(precrec_eval_metrics$prc, glue("{out_dir}/{sample_name}_prc_coordinates.tsv"))
+
+
+}
+
+
