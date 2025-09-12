69519802452cbad589569c00d83977c219bcdff7
MoyukhShabon <moyukh.work@gmail.com>
2025-09-12 17:15:03 +0800

    # minor change to annotation script.
    Now adds the sample_name column so it does not need to be inferred

--- annot/EGAD00001004066/create-annotations.py
+++ annot/EGAD00001004066/create-annotations.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import polars as pl
 
 sample_metadata = pl.read_csv("samples.csv")
@@ -20,20 +19,20 @@ sample_metadata_refined = (
      	"alias" : "sample_alias"
     })
 	.with_columns(
-		pl.col("title").str.split(" ").alias("title_split")
+		pl.col("title").str.split(" ").alias("title_split"),
+		pl.col("title").str.replace_all(" ", "-")
 	)
 	.with_columns(
 		pl.col("title_split").list.get(0).alias("preservation"),
         pl.col("title_split").list.get(1).alias("tissue_type"),
-        pl.col("title_split").list.get(2).alias("sample_type")
+        pl.col("title_split").list.get(2).alias("sample_type"),
+		(pl.col("title") + pl.lit("_") + pl.col("sample_alias")).alias("sample_name")
 	)
 	.drop("title_split")
-	.select(['title','sample_alias','sample_accession_id','preservation','tissue_type','sample_type','phenotype','biological_sex'])
-	.sort(["tissue_type","sample_type","preservation"])
+	.select(['sample_name','sample_alias','sample_accession_id','preservation','tissue_type','sample_type','phenotype','biological_sex'])
 )
 
 sample_metadata_refined.write_csv("sample_annotations.tsv", separator="\t")
-
 
 ### Create fastq annotation file
 
@@ -55,7 +54,6 @@ fastq_annotation = (
 
 fastq_annotation.write_csv("fastq_annotations.tsv", separator="\t")
 
-
 ### Create an annotation file for both samples and fastqs
 
 sample_fastq_annotation = (
@@ -69,7 +67,6 @@ sample_fastq_annotation = (
 )
 
 sample_fastq_annotation.write_csv("sample_fastq_annotations.tsv", separator="\t")
-
 
 ### Generate a count of samples per tissue type
 
