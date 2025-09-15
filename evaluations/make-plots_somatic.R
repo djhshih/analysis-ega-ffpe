039a184413104364ab29549c97d709586985bd92
MoyukhShabon <moyukh.work@gmail.com>
2025-09-15 18:13:32 +0800

    updated plotting code

--- evaluations/make-plots_somatic.R
+++ evaluations/make-plots_somatic.R
@@ -2,7 +2,7 @@
 source("../R/plot.R")
 
 ## Set output directory
-outdir_root = "EGAD00001004066/somatic_vcf/plots"
+outdir_root = "EGAD00001004066/somatic_vcf/plots_publication"
 
 ## Make a vector with paths to all the precrec eval objects
 roc_coord_paths <- sort(list.files("EGAD00001004066/somatic_vcf/roc-prc-auc/precrec", pattern = "all-models_roc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
@@ -29,60 +29,7 @@ for (i in seq_len(length(roc_coord_paths))) {
 	roc_coord <- qread(roc_coord_paths[i])
 	prc_coord <- qread(prc_coord_paths[i])
 
-	roc_plot <- ggplot(roc_coord, aes(x = x, y = y, color = model))  +
-		geom_abline(linetype = "dashed", color = "lightgrey") +
-		geom_line() +
-		labs(
-			title = "ROC",
-			x = "False Positive Rate (1 - Specificity)",
-			y = "True Positive Rate (Sensitivity)",
-			color = "Models"
-		) +
-		coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
-		theme_minimal() +
-		theme(
-			panel.grid.major = element_blank(), # Remove major grid lines
-			panel.grid.minor = element_blank(), # Remove minor grid lines
-			panel.background = element_blank(), # Optional: Remove panel background
-			axis.line = element_line(color = "darkgrey"), # Optional: Add axis lines
-			legend.position = "bottom",
-			plot.title = element_text(hjust = 0.5) # Center the plot title
-		)
-
-	prc_plot <- ggplot(prc_coord, aes(x = x, y = y, color = model))  +
-		geom_line() +
-		labs(
-			title = "PRC",
-			x = "Recall",
-			y = "Precision",
-			color = "Models"
-		) +
-		coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
-		theme_minimal() +
-		theme(
-			panel.grid.major = element_blank(), # Remove major grid lines
-			panel.grid.minor = element_blank(), # Remove minor grid lines
-			panel.background = element_blank(), # Optional: Remove panel background
-			axis.line = element_line(color = "darkgrey"), # Optional: Add axis lines
-			legend.position = "bottom",
-			plot.title = element_text(hjust = 0.5) # Center the plot title
-		)
-
-	roc_prc_plot <- (roc_plot | prc_plot) +
-		plot_annotation(
-			title = "EGAD00001004066 Dataset",
-			subtitle = sample_name,
-			caption = "Only C>T SNVs Evaluated"
-		) +
-		plot_layout(widths = c(1, 1), guides = "collect") &
-		theme(
-			plot.title = element_text(hjust = 0.5, face = "bold", size = 16, margin = margin(t = 10, b = 10)),
-			plot.subtitle = element_text(hjust = 0.5),
-			plot.caption = element_text(hjust = 0),
-			legend.position = "bottom",
-			axis.title.x = element_text(size = 10),
-			axis.title.y = element_text(size = 10)
-		)
+	roc_prc_plot <- make_roc_prc_plot(roc_coord, prc_coord, title = "Bonnet et al., 2018", subtitle = sample_name)
 
 	qdraw(roc_prc_plot, glue("{outdir}/{sample_name}_roc_prc_plot.pdf"), width = 6, height = 5)
 
