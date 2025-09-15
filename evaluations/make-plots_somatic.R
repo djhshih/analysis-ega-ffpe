7a25041b33368ac3641de8f26f1f3b61d31ac8d1
MoyukhShabon <moyukh.work@gmail.com>
2025-09-15 16:55:12 +0800

    added roc prc plotting script

--- evaluations/make-plots_somatic.R
+++ evaluations/make-plots_somatic.R
@@ -1,42 +1,35 @@
-#!/usr/bin/env Rscript
+## This script is used to make ROC and PRC plots from the evaluation results aggregated using combine_results.R
 source("../R/plot.R")
-source("../R/eval.R")
 
 ## Set output directory
 outdir_root = "EGAD00001004066/somatic_vcf/plots"
 
 ## Make a vector with paths to all the precrec eval objects
-precrec_roc_coord <- Sys.glob("EGAD00001004066/somatic_vcf/roc-prc-auc/precrec/*/*roc_coordinates.tsv")
-precrec_prc_coord <- Sys.glob("EGAD00001004066/somatic_vcf/roc-prc-auc/precrec/*/*prc_coordinates.tsv")
+roc_coord_paths <- sort(list.files("EGAD00001004066/somatic_vcf/roc-prc-auc/precrec", pattern = "all-models_roc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
+prc_coord_paths <- sort(list.files("EGAD00001004066/somatic_vcf/roc-prc-auc/precrec", pattern = "all-models_prc_coordinates.tsv", recursive = TRUE, full.names = TRUE))
 
+## Plot display size parameters for debugging
+# options(repr.plot.width = 6, repr.plot.height = 5)
 
 ## Create plots for each of the evaluated samples
-print(glue("Creating ROC PRC plot for:"))
+message("Creating ROC PRC plot for:")
 
-outdir = glue("{outdir_root}/roc_prc_plots/")
+outdir = glue("{outdir_root}/roc_prc_plots")
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
 
-for (i in seq_len(length(precrec_roc_coord))) {
+for (i in seq_len(length(roc_coord_paths))) {
 
-	sample_name_roc <- unlist(strsplit(precrec_roc_coord[i], "/"))[5]
-	sample_name_prc <- unlist(strsplit(precrec_prc_coord[i], "/"))[5]
-	
-	if (sample_name_roc != sample_name_prc) {
-		stop(paste("Mismatch found at index", i, ": ROC sample '", 
-			sample_name_roc, "' does not match PRC sample '", 
-			sample_name_prc, "'. Halting execution.", sep=""))
-	}
-	
-	print(glue("\t {i}. {sample_name_roc}"))
+	sample_name <- match_return_sample_name(roc_coord_paths[i], prc_coord_paths[i])
+	message(glue("\t {i}. {sample_name}"))
 
 	# ## Read in the variant set to count number and annotate the number of SNVs in the plot
 	# eval_snv_set <- qread(glue("EGAD00001004066/somatic_vcf/model-scores_truths/{sample_name}/{sample_name}_model-scores_truths.tsv"))
 	# snv_count <- nrow(eval_snv_set)
 
-	roc_coord <- qread(precrec_roc_coord[i])
-	prc_coord <- qread(precrec_prc_coord[i])
+	roc_coord <- qread(roc_coord_paths[i])
+	prc_coord <- qread(prc_coord_paths[i])
 
-	roc_plot <- ggplot(roc_coord, aes(x = x, y = y, color = modname))  +
+	roc_plot <- ggplot(roc_coord, aes(x = x, y = y, color = model))  +
 		geom_abline(linetype = "dashed", color = "lightgrey") +
 		geom_line() +
 		labs(
@@ -56,8 +49,7 @@ for (i in seq_len(length(precrec_roc_coord))) {
 			plot.title = element_text(hjust = 0.5) # Center the plot title
 		)
 
-	prc_plot <- ggplot(prc_coord, aes(x = x, y = y, color = modname))  +
-		geom_abline(linetype = "dashed", color = "lightgrey") +
+	prc_plot <- ggplot(prc_coord, aes(x = x, y = y, color = model))  +
 		geom_line() +
 		labs(
 			title = "PRC",
@@ -79,7 +71,7 @@ for (i in seq_len(length(precrec_roc_coord))) {
 	roc_prc_plot <- (roc_plot | prc_plot) +
 		plot_annotation(
 			title = "EGAD00001004066 Dataset",
-			subtitle = sample_name_roc,
+			subtitle = sample_name,
 			caption = "Only C>T SNVs Evaluated"
 		) +
 		plot_layout(widths = c(1, 1), guides = "collect") &
@@ -92,6 +84,6 @@ for (i in seq_len(length(precrec_roc_coord))) {
 			axis.title.y = element_text(size = 10)
 		)
 
-	qdraw(roc_prc_plot, glue("{outdir}/{sample_name_roc}_roc_prc_plot.pdf"), width = 7, height = 5)
+	qdraw(roc_prc_plot, glue("{outdir}/{sample_name}_roc_prc_plot.pdf"), width = 6, height = 5)
 
 }
