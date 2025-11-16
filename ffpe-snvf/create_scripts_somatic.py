8d475c5f07ff79637bac19d7a16fc44e983b9a12
MoyukhShabon <moyukh.work@gmail.com>
2025-11-17 01:13:20 +0800

    bug fix

--- ffpe-snvf/create_scripts_somatic.py
+++ ffpe-snvf/create_scripts_somatic.py
@@ -14,13 +14,11 @@ dataset = "EGAD00001004066"
 ref_path = return_path_if_exists("../data/ref/Homo_sapiens_assembly38.fasta", abs=True)
 
 vcf_paths = [return_path_if_exists(path, abs=True) for path in glob.glob(f"../vcf/{dataset}/somatic_vcf/*/*.vcf.gz")]
-filtered_outdir = os.path.abspath("somatic_vcf")
+
+filtered_outdir = os.path.abspath(f"{dataset}/somatic_vcf")
+
 
 models = ["mobsnvf", "vafsnvf", "sobdetector", "ideafix"]
-
-outdir = "batch_scripts"
-os.makedirs(outdir, exist_ok=True)
-outdir = return_path_if_exists(outdir, abs=True)
 
 for vcf_path in vcf_paths:
     
@@ -36,7 +34,7 @@ for vcf_path in vcf_paths:
                     
             ## Template script for each model
             template_dir = return_path_if_exists(f"{model}.R", abs=True)
-            content = f"#!/usr/bin/env Rscript\nRscript {template_dir} --vcf '{vcf_path}' --ref '{ref_path}' --outdir '{filtered_outdir}' '{ref_path}'\n"
+            content = f"#!/usr/bin/env Rscript\nRscript {template_dir} --vcf '{vcf_path}' --ref '{ref_path}' --outdir '{filtered_outdir}'\n"
             filename = f"{outdir}/{model}_{sample_name}.sh"
             
             with open(filename, "w") as f:
@@ -48,7 +46,7 @@ for vcf_path in vcf_paths:
             os.makedirs(outdir, exist_ok=True)
         
             template_dir = return_path_if_exists(f"{model}.sh.template", abs=True)
-            content = f"#!/bin/bash\nbash {template_dir} {bam_path} '../{vcf_path}' '../{filtered_outdir}'\n"
+            content = f"#!/bin/bash\nbash {template_dir} {bam_path} '../{vcf_path}' '../{filtered_outdir}'  '{ref_path}'\n"
             filename = f"{outdir}/{model}_{sample_name}.sh"
             
             with open(filename, "w") as f:
