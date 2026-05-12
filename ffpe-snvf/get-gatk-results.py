f2ec3e1b29fa30c0146ce4c719345f2727b0581b
MoyukhShabon <moyukh.work@gmail.com>
2026-05-12 20:40:57 +0800

    updated gatk result fetching script

--- ffpe-snvf/get-gatk-results.py
+++ ffpe-snvf/get-gatk-results.py
@@ -1,15 +1,25 @@
 import polars as pl
 from pathlib import Path
-import sys
+import vcformer as vcf
 from tqdm import tqdm
 
 repo_root = Path("..")
 
-## Local Dependencies
-sys.path.append(str(repo_root / "common-ffpe-snvf/python"))
-from common import read_variants
-
 ## Functions
+def get_gatk_obmm(vcf_path: Path) -> pl.DataFrame:
+	"""Reads VCF and returns the GATK Oritentation Bias mixture model's probability score and prediction"""
+	gatk_obmm = (
+		vcf.read_vcf_as_polars(vcf_path, info_fields=["ROQ"], sample_fields=[])
+		.explode("filters", "alts")
+		.rename({"filters" : "filter", "alts" : "alt"})
+		.rename(lambda x: x.lower())
+		.select("chrom", "pos", "ref", "alt", "filter", "roq")
+		.with_columns(obmm_prob = 1 - (10 ** ( -pl.col("roq") / 10 )))
+	)
+
+	return gatk_obmm
+
+
 def process_dataset(dataset: str = "EGAD00001004066", variant_set: str = "somatic_filtered") -> None:
 	"""Read the variant and filter columns from VCF and write 
 	a table for evaluating the gatk orientation bias mixture model.
@@ -18,6 +28,8 @@ def process_dataset(dataset: str = "EGAD00001004066", variant_set: str = "somati
 		dataset: Dataset identifier (e.g. 'EGAD00001004066').
 		variant_set: Variant set folder name (e.g. 'somatic_filtered').
 	"""
+	print(f"Processing Dataset : {dataset} | Variant Set : {variant_set}")
+
 	outdir_root = (repo_root / "ffpe-snvf" / dataset / variant_set / "gatk-obmm")
 	vcf_paths = list((repo_root / "vcf" / dataset / variant_set).glob("*/*FFPE*.vcf.gz"))
 
@@ -26,7 +38,7 @@ def process_dataset(dataset: str = "EGAD00001004066", variant_set: str = "somati
 		outdir = (outdir_root / sample_name)
 		outdir.mkdir(exist_ok=True, parents=True)
 
-		gatk_res = read_variants(path, columns=["#CHROM", "POS", "REF", "ALT", "FILTER"])
+		gatk_res = get_gatk_obmm(path)
 		gatk_res.write_csv((outdir / sample_name).with_suffix(".gatk-obmm.tsv"), separator="\t")
 
 ## Get GATK OBMM results
